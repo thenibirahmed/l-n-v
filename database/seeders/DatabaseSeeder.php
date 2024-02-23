0838c298a639299c36504c4bf36f00f8cbee2c8f
Nibir Ahmed <nibirahmed9611@gmail.com>
2024-02-23 15:32:41 +0600

    vue router with fresh laravel

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,11 +18,5 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
-
-        // \App\Models\Counter::factory(1)->create();
-        // \App\Models\Product::factory(5)->create();
-        // \App\Models\Customer::factory(5)->create();
-        // \App\Models\Invoice::factory(5)->create();
-        // \App\Models\InvoiceItem::factory(5)->create();
     }
 }
